e804cd57ed9a8060ac021a6aba96072951569189
zxdual <zxdual@gmail.com>
2016-11-13 14:31:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function() {
+	function getRandomInt(min, max) {
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+	
+	var quotes = ['"Injustice anywhere is a threat to justice everywhere." - Martin Luther King, Jr.',
+		      '"Where justice is denied, where poverty is enforced, where ignorance prevails, and where any one class is made to feel that society is an organized conspiracy to oppress, rob and degrade them, neither persons nor property will be safe." - Frederick Douglas',
+		      '"I\'m for truth, no matter who tells it. I\'m for justice, no matter who it\'s for or against." - Malcom X',
+		      '"Peace cannot be kept by force; it can only be achieved by understanding." - Albert Einstein',
+		      ' "Peace is a journey of a thousand miles and it must be taken one step at a time." - Lyndon B. Johnson'
+	];
+	
+	var random = getRandomInt(0, quotes.length);
+
+	$('#quote-text').text(quotes[random]);
+});
